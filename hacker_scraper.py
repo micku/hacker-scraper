4c55e8192c54e924893d5f0e5604efa97ac123e0
Michele Gargiulo <michele@grg.io>
2016-09-06 22:22:06 +0200

    Adds input parameters checking Adds entry point docstring

--- hacker_scraper.py
+++ hacker_scraper.py
@@ -4,8 +4,14 @@ import click
 
 
 @click.command()
-@click.option('--posts', default=10, help='Number of posts to scrape.')
+@click.option('--posts',
+    default=10,
+    help='Number of posts to scrape.',
+    type=click.IntRange(
+        min=0,
+        max=100))
 def hacker_scraper(posts):
+    """This is a simple CLI applications that scrapes data from Hacker News."""
     click.echo('Number of posts: {}'.format(posts))
 
 
