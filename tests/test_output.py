349dcee1bb601a43a67bd752486401e0b5a16dba
Michele Gargiulo <michele@grg.io>
2016-09-10 16:11:27 +0200

    Adds asserts on JSON parsing

--- tests/test_output.py
+++ tests/test_output.py
@@ -11,19 +11,32 @@ class TestOutput(unittest.TestCase):
         runner = CliRunner()
 
         result = runner.invoke(hacker_scraper, [])
-        self.assertGreaterEqual(result.exit_code, 0)
-        js = json.loads(result.output)
+        self.assertGreaterEqual(result.exit_code, 0,
+            'Output is: {}'.format(result.output))
+        try:
+            js = json.loads(result.output)
+        except ValueError:
+            self.fail('Output is not a valid JSON')
 
 
     def test_quantity(self):
         runner = CliRunner()
 
         result = runner.invoke(hacker_scraper, [])
-        self.assertGreaterEqual(result.exit_code, 0)
-        js = json.loads(result.output)
+        self.assertGreaterEqual(result.exit_code, 0,
+            'Output is: {}'.format(result.output))
+        try:
+            js = json.loads(result.output)
+        except ValueError:
+            self.fail('Output is not a valid JSON')
         self.assertEqual(len(js), 10)
 
         result = runner.invoke(hacker_scraper, ['--posts', '13'])
-        self.assertGreaterEqual(result.exit_code, 0)
-        js = json.loads(result.output)
-        self.assertEqual(len(js), 13)
+        self.assertGreaterEqual(result.exit_code, 0,
+            'Output is: {}'.format(result.output))
+        try:
+            js = json.loads(result.output)
+        except ValueError:
+            self.fail('Output is not a valid JSON')
+        self.assertEqual(len(js), 13,
+            'Output is: {}'.format(result.output))
